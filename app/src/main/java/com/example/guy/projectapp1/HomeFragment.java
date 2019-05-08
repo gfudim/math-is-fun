b071267251c5888f26cc239ab8fa99faaf4c4dfb
guymoz84 <guymozes@gmail.com>
2019-05-08 20:07:36 +0300

    Only basic version for loading data from firebase

--- app/src/main/java/com/example/guy/projectapp1/HomeFragment.java
+++ app/src/main/java/com/example/guy/projectapp1/HomeFragment.java
@@ -14,20 +14,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-//import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
-
-//import java.util.ArrayList;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import io.paperdb.Paper;
@@ -37,13 +31,12 @@ import static com.example.guy.projectapp1.Utils.MULTI_MODE;
 import static com.example.guy.projectapp1.Utils.SEARCH_MODE;
 import static com.example.guy.projectapp1.Utils.SINGLE_MODE;
 import static com.example.guy.projectapp1.Utils.user;
-import static com.example.guy.projectapp1.Utils.databaseUsers;
 import static com.example.guy.projectapp1.Utils.id_for_user;
 
 public class HomeFragment extends Fragment {
     Button startBtn;
     Boolean cleaned_exercises = false;
-    private DatabaseReference ref;
+    private DatabaseReference reff;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -55,13 +48,120 @@ public class HomeFragment extends Fragment {
             id_for_user++;
             Log.e(TAG,String.format("id_for_user %s",user.id_data_base));
         }
-        if (user.mode == MULTI_MODE && false){//TODO - added a false to bypass the crash on database.getReference(user_path);
-            ref = databaseUsers.child(user.id_data_base);
-            ref.addValueEventListener(new ValueEventListener() {
+        if (user.mode == MULTI_MODE){//TODO - added a false to bypass the crash on database.getReference(user_path);
+            reff = FirebaseDatabase.getInstance().getReference().child("user").child(user.id_data_base);
+            reff.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    user = dataSnapshot.getValue(User.class);
-                    Log.e(TAG,String.format("222222 %s", user.total_answers));
+                    int correct_answers = Integer.parseInt(dataSnapshot.child("correct_answers").getValue().toString());
+                    int count_tests = Integer.parseInt(dataSnapshot.child("count_tests").getValue().toString());
+                    int current_correct_tests_in_row = Integer.parseInt(dataSnapshot.child("current_correct_tests_in_row").getValue().toString());
+                    int current_count_points_per_day = Integer.parseInt(dataSnapshot.child("current_count_points_per_day").getValue().toString());
+
+                    user.correct_answers = correct_answers;
+                    user.count_tests = count_tests;
+                    user.current_correct_tests_in_row = current_correct_tests_in_row;
+                    user.current_count_points_per_day = current_count_points_per_day;
+
+                    // current exercises
+                    // ex1
+                    int count_correct_answers = Integer.parseInt(dataSnapshot.child("current_exercises").child("0").child("count_correct_answers").getValue().toString());
+                    int count_wrong_answers = Integer.parseInt(dataSnapshot.child("current_exercises").child("0").child("count_wrong_answers").getValue().toString());
+                    Boolean displayed_today = Boolean.valueOf(dataSnapshot.child("current_exercises").child("0").child("displayed_today").getValue().toString());
+                    int exercise_id = Integer.parseInt(dataSnapshot.child("current_exercises").child("0").child("exercise_id").getValue().toString());
+                    int mul1 = Integer.parseInt(dataSnapshot.child("current_exercises").child("0").child("mul1").getValue().toString());
+                    int mul2 = Integer.parseInt(dataSnapshot.child("current_exercises").child("0").child("mul2").getValue().toString());
+                    long time_answered = Long.parseLong(dataSnapshot.child("current_exercises").child("0").child("time_answered").getValue().toString());
+                    long time_displayed = Long.parseLong(dataSnapshot.child("current_exercises").child("0").child("time_displayed").getValue().toString());
+                    Exercise current_exercises_ex1 = new Exercise(mul1,mul2,exercise_id);
+                    current_exercises_ex1.count_correct_answers = count_correct_answers;
+                    current_exercises_ex1.count_wrong_answers = count_wrong_answers;
+                    current_exercises_ex1.displayed_today = displayed_today;
+                    current_exercises_ex1.time_answered = time_answered;
+                    current_exercises_ex1.time_displayed = time_displayed;
+
+                    // ex2
+                    count_correct_answers = Integer.parseInt(dataSnapshot.child("current_exercises").child("1").child("count_correct_answers").getValue().toString());
+                    count_wrong_answers = Integer.parseInt(dataSnapshot.child("current_exercises").child("1").child("count_wrong_answers").getValue().toString());
+                    displayed_today = Boolean.valueOf(dataSnapshot.child("current_exercises").child("1").child("displayed_today").getValue().toString());
+                    exercise_id = Integer.parseInt(dataSnapshot.child("current_exercises").child("1").child("exercise_id").getValue().toString());
+                    mul1 = Integer.parseInt(dataSnapshot.child("current_exercises").child("1").child("mul1").getValue().toString());
+                    mul2 = Integer.parseInt(dataSnapshot.child("current_exercises").child("1").child("mul2").getValue().toString());
+                    time_answered = Long.parseLong(dataSnapshot.child("current_exercises").child("1").child("time_answered").getValue().toString());
+                    time_displayed = Long.parseLong(dataSnapshot.child("current_exercises").child("1").child("time_displayed").getValue().toString());
+                    Exercise current_exercises_ex2 = new Exercise(mul1,mul2,exercise_id);
+                    current_exercises_ex2.count_correct_answers = count_correct_answers;
+                    current_exercises_ex2.count_wrong_answers = count_wrong_answers;
+                    current_exercises_ex2.displayed_today = displayed_today;
+                    current_exercises_ex2.time_answered = time_answered;
+                    current_exercises_ex2.time_displayed = time_displayed;
+
+                    // ex3
+                    count_correct_answers = Integer.parseInt(dataSnapshot.child("current_exercises").child("2").child("count_correct_answers").getValue().toString());
+                    count_wrong_answers = Integer.parseInt(dataSnapshot.child("current_exercises").child("2").child("count_wrong_answers").getValue().toString());
+                    displayed_today = Boolean.valueOf(dataSnapshot.child("current_exercises").child("2").child("displayed_today").getValue().toString());
+                    exercise_id = Integer.parseInt(dataSnapshot.child("current_exercises").child("2").child("exercise_id").getValue().toString());
+                    mul1 = Integer.parseInt(dataSnapshot.child("current_exercises").child("2").child("mul1").getValue().toString());
+                    mul2 = Integer.parseInt(dataSnapshot.child("current_exercises").child("2").child("mul2").getValue().toString());
+                    time_answered = Long.parseLong(dataSnapshot.child("current_exercises").child("2").child("time_answered").getValue().toString());
+                    time_displayed = Long.parseLong(dataSnapshot.child("current_exercises").child("2").child("time_displayed").getValue().toString());
+                    Exercise current_exercises_ex3 = new Exercise(mul1,mul2,exercise_id);
+                    current_exercises_ex3.count_correct_answers = count_correct_answers;
+                    current_exercises_ex3.count_wrong_answers = count_wrong_answers;
+                    current_exercises_ex3.displayed_today = displayed_today;
+                    current_exercises_ex3.time_answered = time_answered;
+                    current_exercises_ex3.time_displayed = time_displayed;
+
+                    // ex4
+                    count_correct_answers = Integer.parseInt(dataSnapshot.child("current_exercises").child("3").child("count_correct_answers").getValue().toString());
+                    count_wrong_answers = Integer.parseInt(dataSnapshot.child("current_exercises").child("3").child("count_wrong_answers").getValue().toString());
+                    displayed_today = Boolean.valueOf(dataSnapshot.child("current_exercises").child("3").child("displayed_today").getValue().toString());
+                    exercise_id = Integer.parseInt(dataSnapshot.child("current_exercises").child("3").child("exercise_id").getValue().toString());
+                    mul1 = Integer.parseInt(dataSnapshot.child("current_exercises").child("3").child("mul1").getValue().toString());
+                    mul2 = Integer.parseInt(dataSnapshot.child("current_exercises").child("3").child("mul2").getValue().toString());
+                    time_answered = Long.parseLong(dataSnapshot.child("current_exercises").child("3").child("time_answered").getValue().toString());
+                    time_displayed = Long.parseLong(dataSnapshot.child("current_exercises").child("3").child("time_displayed").getValue().toString());
+                    Exercise current_exercises_ex4 = new Exercise(mul1,mul2,exercise_id);
+                    current_exercises_ex4.count_correct_answers = count_correct_answers;
+                    current_exercises_ex4.count_wrong_answers = count_wrong_answers;
+                    current_exercises_ex4.displayed_today = displayed_today;
+                    current_exercises_ex4.time_answered = time_answered;
+                    current_exercises_ex4.time_displayed = time_displayed;
+
+                    ArrayList <Exercise> temp_group = new ArrayList<>();
+                    temp_group.add(current_exercises_ex1);
+                    temp_group.add(current_exercises_ex2);
+                    temp_group.add(current_exercises_ex3);
+                    temp_group.add(current_exercises_ex4);
+                    user.current_exercises = temp_group;
+
+                    int days_in_row = Integer.parseInt(dataSnapshot.child("days_in_row").getValue().toString());
+                    int lang = Integer.parseInt(dataSnapshot.child("lang").getValue().toString());
+                    int last_day_of_session = Integer.parseInt(dataSnapshot.child("last_day_of_session").getValue().toString());
+                    int max_correct_tests_in_row = Integer.parseInt(dataSnapshot.child("max_correct_tests_in_row").getValue().toString());
+                    int max_points_per_day = Integer.parseInt(dataSnapshot.child("max_points_per_day").getValue().toString());
+                    int mode = Integer.parseInt(dataSnapshot.child("mode").getValue().toString());
+                    user.days_in_row = days_in_row;
+                    user.lang = lang;
+                    user.last_day_of_session = last_day_of_session;
+                    user.max_correct_tests_in_row = max_correct_tests_in_row;
+                    user.max_points_per_day = max_points_per_day;
+                    user.mode = mode;
+                    Boolean search_exercises_done = Boolean.valueOf(dataSnapshot.child("search_exercises_done").getValue().toString());
+                    Boolean session_done = Boolean.valueOf(dataSnapshot.child("session_done").getValue().toString());
+                    int session_type = Integer.parseInt(dataSnapshot.child("session_type").getValue().toString());
+                    Boolean start_page = Boolean.valueOf(dataSnapshot.child("start_page").getValue().toString());
+                    int tests_in_row = Integer.parseInt(dataSnapshot.child("tests_in_row").getValue().toString());
+                    int total_answers = Integer.parseInt(dataSnapshot.child("total_answers").getValue().toString());
+                    user.search_exercises_done = search_exercises_done;
+                    user.session_done = session_done;
+                    user.session_type = session_type;
+                    user.start_page = start_page;
+                    user.tests_in_row = tests_in_row;
+                    user.total_answers = total_answers;
+                    int wrong_answers = Integer.parseInt(dataSnapshot.child("wrong_answers").getValue().toString());
+                    user.wrong_answers = wrong_answers;
+                    Log.e(TAG,String.format("222222 ex2: %s",current_exercises_ex3.exercise_id));
                 }
                 @Override
                 public void onCancelled(@NonNull DatabaseError databaseError) {
@@ -122,41 +222,7 @@ public class HomeFragment extends Fragment {
         });
         return view;
     }
-//
-//    private void updateUser(DataSnapshot dataSnapshot) {
-//        for(DataSnapshot ds: dataSnapshot.getChildren()){
-//            User temp_user = new User(MULTI_MODE);
-//            temp_user.setCorrectAnswers(ds.child(user.id_data_base).getValue(User.class).correct_answers);
-//            temp_user.setWrongAnswers(ds.child(user.id_data_base).getValue(User.class).wrong_answers);
-//            temp_user.setTotalAnswers(ds.child(user.id_data_base).getValue(User.class).total_answers);
-//            temp_user.setMode(ds.child(user.id_data_base).getValue(User.class).mode);
-//            temp_user.setLang(ds.child(user.id_data_base).getValue(User.class).lang);
-//            temp_user.setAge(ds.child(user.id_data_base).getValue(User.class).age);
-//            temp_user.setSessionType(ds.child(user.id_data_base).getValue(User.class).session_type);
-//            temp_user.setMaxCorrectTestsInRow(ds.child(user.id_data_base).getValue(User.class).max_correct_tests_in_row);
-//            temp_user.setCurrentCorrectTestsInRow(ds.child(user.id_data_base).getValue(User.class).current_correct_tests_in_row);
-//            temp_user.setCountTests(ds.child(user.id_data_base).getValue(User.class).count_tests);
-//            temp_user.setCurrentCountPointsPerDay(ds.child(user.id_data_base).getValue(User.class).current_count_points_per_day);
-//            temp_user.setMaxPointsPerDay(ds.child(user.id_data_base).getValue(User.class).max_points_per_day);
-//            temp_user.setFirstLogin(ds.child(user.id_data_base).getValue(User.class).first_login);
-//            temp_user.setLastLogin(ds.child(user.id_data_base).getValue(User.class).last_login);
-//            temp_user.setStartSessionTime(ds.child(user.id_data_base).getValue(User.class).start_session_time);
-//            temp_user.setStartExercise(ds.child(user.id_data_base).getValue(User.class).start_exercise);
-//            temp_user.setEndExercise(ds.child(user.id_data_base).getValue(User.class).end_exercise);
-//            temp_user.setSessionDone(ds.child(user.id_data_base).getValue(User.class).session_done);
-//            temp_user.setStartPage(ds.child(user.id_data_base).getValue(User.class).start_page);
-//            temp_user.setName(ds.child(user.id_data_base).getValue(User.class).name);
-//            temp_user.setName(ds.child(user.id_data_base).getValue(User.class).name);
-//            temp_user.setIdDataBase(ds.child(user.id_data_base).getValue(User.class).id_data_base);
-//            temp_user.setKnownExercises(ds.child(user.id_data_base).getValue(User.class).known_exercises);
-//            temp_user.setUnknownExercises(ds.child(user.id_data_base).getValue(User.class).unknown_exercises);
-//            temp_user.setUndefinedExercises(ds.child(user.id_data_base).getValue(User.class).undefined_exercises);
-//            temp_user.setCurrentExercises(ds.child(user.id_data_base).getValue(User.class).current_exercises);
-//            // user = temp_user;
-//            Toast.makeText(getActivity(),String.format("total_answers%s", temp_user.total_answers),Toast.LENGTH_LONG).show(); // TODO - remove (only for debug)
-//        }
-//    }
-//
+
     public void uncheckDisplayedExercises(){
         int i;
         for (i=0;i<user.current_exercises.size(); i++){
